c7c0bfd71baab2fcc0fa64aef6c41bd43179641d
shunya sago <shun.s0029@gmail.com>
2021-10-20 08:18:40 +0000

    deploy setting

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c4d37499b6699333c474b3e1007d3c7c21cecf211526598fd56c7aec6d166fbdba92456334d47fbf2369854784ec9ce83b2f9bdff886a32b7b43482219ec90b4'
