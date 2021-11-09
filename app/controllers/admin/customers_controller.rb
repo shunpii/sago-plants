12f4ee761a0abbe620847b5719c654b9b0ec9fe3
shunya sago <shun.s0029@gmail.com>
2021-11-09 02:19:57 +0000

    [Fix]customer/ステータスの編集

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -19,6 +19,6 @@ class Admin::CustomersController < ApplicationController
 
   private
   def customer_params
-    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :email)
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :email, :is_member)
   end
 end
